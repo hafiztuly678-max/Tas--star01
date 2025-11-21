dd92e36d518a6be60fac2d667ea738163c1850e2
Tasmim Hafiz <hafiztuly678@gmail.com>
2025-11-21 17:20:56 -0500

    Improve formatting of Pascal's triangle output
    Refactor printing of Pascal's triangle for clarity.

--- pascal.py
+++ pascal.py
@@ -1,4 +1,3 @@
-
 def pascal(n):
     triangle = [[1]]
     for i in range(1, n):
@@ -7,18 +6,13 @@ def pascal(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-        
     return triangle
-
 
 question = int(input("How many levels do you want to generate? "))
 print("Pascal's triangle with", question, "levels:")
-print(pascal(question))
-
-pyramid = pascal(question) 
-
+pyramid = pascal(question)
 for row in pyramid:
-    print(" " * (question - len(row)), *row)       # loops through each row of the triangle. For each row, it prints spaces 
+    print(" " * (question - len(row)), *row)# loops through each row of the triangle. For each row, it prints spaces 
                                                     #first, then the numbers. len(row) counts how many numbers are in the row. 
                                                     # question - len(row) calculates how many spaces to add. Shorter rows get more spaces, 
                                                     # longer rows get fewer. 
