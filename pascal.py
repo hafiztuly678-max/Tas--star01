2d7f96e16c90a157b5211cbec63a9681bf1c25bd
Tasmim Hafiz <hafiztuly678@gmail.com>
2025-11-21 17:11:20 -0500

    Create pascal.py

---
+++ pascal.py
@@ -0,0 +1,24 @@
+
+def pascal(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+        
+    return triangle
+
+
+question = int(input("How many levels do you want to generate? "))
+print("Pascal's triangle with", question, "levels:")
+print(pascal(question))
+
+pyramid = pascal(question) 
+
+for row in pyramid:
+    print(" " * (question - len(row)), *row)       # loops through each row of the triangle. For each row, it prints spaces 
+                                                    #first, then the numbers. len(row) counts how many numbers are in the row. 
+                                                    # question - len(row) calculates how many spaces to add. Shorter rows get more spaces, 
+                                                    # longer rows get fewer. 
